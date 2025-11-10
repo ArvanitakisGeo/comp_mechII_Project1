86c4490f6af59ba37e0bfd7e70643f9bbcc7c7fd
Geogios <garvan03@gmail.com>
2025-11-10 10:46:36 +0200

    μmaybe finall

---
+++ Solver.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+The SEMFE Heat Transfer Solver
+Computational Mechanics
+
+Solver Script
+"""
+
+# --------------------------
+# File: fem.py
+# --------------------------
+import scipy.sparse.linalg as spla
+import numpy as np
+from scipy.sparse import coo_matrix
+
+
+def element_stiffness_triangle(node_coords, k=1.0):
+    """
+    Linear triangular element stiffness for steady-state conduction (Poisson equation)
+    node_coords: (3,2) or (3,3) array of node coordinates
+    returns 3x3 element stiffness matrix
+    """
+    x1, y1 = node_coords[0, 0], node_coords[0, 1]
+    x2, y2 = node_coords[1, 0], node_coords[1, 1]
+    x3, y3 = node_coords[2, 0], node_coords[2, 1]
+    
+    area = 0.5 * np.linalg.det( np.array([
+    [1, x1, y1],
+    [1, x2, y2],
+    [1, x3, y3],
+]) )# element area
+
+    # Shape function derivatives (constant over element)
+    B =(1/(2*area)) * np.array([
+    [y2-y3, y3-y1, y1-y2],
+    [x3-x2, x1-x3, x2-x1],]) #matrix B
+    Ke = k * area * (B.T @ B)
+    return Ke
+
+
+def assemble_global(nodes, elems, k=1.0):
+    """
+    Assemble global stiffness matrix for triangular mesh
+    nodes: Nx2 or Nx3 array
+    elems: Mx3 array of node indices (0-based)
+    k: thermal conductivity
+    returns: K (sparse CSR matrix)
+    """
+    nnodes = nodes.shape[0]
+    nelems = elems.shape[0]
+    rows = []
+    cols = []
+    data = []
+
+    for e in range(nelems):
+        conn = elems[e] #elem array
+        coords = nodes[conn, :2]  # take x,y only from every node in elem
+        Ke = element_stiffness_triangle(coords, k=k)
+        for i_local, i_global in enumerate(conn):
+            for j_local, j_global in enumerate(conn):
+                rows.append(i_global)#paste in rows the value i_global
+                cols.append(j_global)
+                data.append(Ke[i_local, j_local])
+
+    # build sparse matrix (COO(sparse) -> CSR(usefull to solve later)) 
+    K = coo_matrix((data, (rows, cols)), shape=(nnodes, nnodes)).tocsr()# with .tocsr() change coo->csr
+                # Assemble global Matrix K
+    return K
+
+def apply_dirichlet(K, f, bc_nodes, bc_values):
+
+    K = K.tolil(copy=True)
+    f = f.copy()
+
+
+    for node, val in zip(bc_nodes, bc_values):
+        
+        col = K[:, node].toarray().ravel()
+        f -= col * val
+
+        K.rows[node] = []
+        K.data[node] = []
+        K[:, node] = 0
+        K[node, node] = 1.0
+        f[node] = val                 
+
+    return K.tocsr(), f
+
+def apply_heat_flux(f, nodes, elems, heat_flux_bcs):
+    """
+    Apply Neumann (heat flux) BCs to load vector.
+    Each BC: (elem_id, edge_id, q)
+    """
+    for elem_id, edge_id, q in heat_flux_bcs:
+        conn = elems[elem_id]
+        coords = nodes[conn, :2]
+        edge_nodes = { 1: [0,1], 2: [1,2], 3: [2,0]}[edge_id]
+        n1, n2 = edge_nodes
+        x1, y1 = coords[n1]
+        x2, y2 = coords[n2]
+        L = np.hypot(x2-x1, y2-y1)
+        fe = q*L*np.array([0.5,0.5])# linear edge shape functions
+        f[conn[edge_nodes]] += fe
+    return f
+
+
+def apply_convection(K, f, nodes, elems, conv_bcs):
+    """
+    Apply Robin (convection) BCs to load vector & matrix K.
+    Each BC: (elem_id, edge_id, h, Tinf)
+    """
+    for elem_id, edge_id, h, Tinf in conv_bcs:
+        conn = elems[elem_id]
+        coords = nodes[conn, :2]
+        edge_nodes = {
+            1: [0,1],
+            2: [1,2],
+            3: [2,0]
+        }[edge_id]
+        n1, n2 = edge_nodes
+        x1, y1 = coords[n1]
+        x2, y2 = coords[n2]
+      
+        L = np.hypot(x2 - x1, y2 - y1)
+        
+        # Contribution to K (2x2)
+        Ke_edge = (h * L / 6) * np.array([[2,1],
+                                          [1,2]])
+        # Contribution to f (2x1)
+        fe_edge = (h * L / 2) * np.array([1.0, 1.0]) * Tinf
+        
+        # Προσθήκη στα σωστά DOF
+        for i_local, i_global in enumerate([n1,n2]):
+            f[conn[i_global]] += fe_edge[i_local]
+            for j_local, j_global in enumerate([n1,n2]):
+                K[conn[i_global], conn[j_global]] += Ke_edge[i_local, j_local]
+                
+    return K, f
+
+
+def solve_system(K, f):
+    """Solve the linear system Ku=f"""
+    u = spla.spsolve(K, f)
+    return u
---
+++ main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+The SEMFE Heat Transfer Solver
+Computational Mechanics
+
+Main Script
+"""
+import numpy as np
+from PreProcessor import read_input_file
+from Solver import assemble_global, apply_convection, apply_dirichlet
+from Solver import apply_heat_flux, solve_system
+from PostProcessor import plot_mesh, plot_mesh_interactive, plot_temperature_field
+from PostProcessor import export_temperature_csv
+
+
+# Import model info
+nodes, elems, materials, k, bcs = read_input_file('validation.xml')
+
+# Check Mesh Quality
+plot_mesh_interactive(nodes, elems, show=True, filename='interactive_mesh3.html')
+
+# Assemble global
+K = assemble_global(nodes, elems, k=k)
+
+# Apply BCs
+
+# Dirichlet
+bc_nodes  = [node for (node, val) in bcs.get('temperature', [])]
+bc_values = [val  for (node, val) in bcs.get('temperature', [])]
+
+# Convection (Robin): (elem_id, edge_id, h, Tinf)
+bc_conv = [(elem_id, edge_id, h, Tinf)
+            for (elem_id, edge_id, h, Tinf) in bcs.get('convection', [])]
+
+# Heat flux (Neumann): (elem_id, edge_id, q)
+bc_heat_flux = [(elem_id, edge_id, q)
+                 for (elem_id, edge_id, q) in bcs.get('heat_flux', [])]
+f0 = np.zeros(nodes.shape[0])
+
+# 1) Convection
+K1, f1 = apply_convection(K, f0, nodes, elems, bc_conv)
+
+# 2) Heat flux
+f2 = apply_heat_flux(f1, nodes, elems, bc_heat_flux)
+
+# 3) Dirichlet στο τέλος
+Kmod, fmod = apply_dirichlet(K1, f2, bc_nodes, bc_values)
+
+# Kmod, fmod = apply_dirichlet(K, np.zeros(nodes.shape[0]), bc_nodes, bc_values)
+# Kmod, fmod = apply_convection(K, fmod, nodes, elems, bc_values)
+
+#
+u = solve_system(Kmod, fmod)
+
+# Call it in main
+plot_temperature_field(nodes, elems, u, filename='temperature_field.png')
+export_temperature_csv(nodes, u)
